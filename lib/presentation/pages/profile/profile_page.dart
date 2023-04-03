06fb87a5722f273e4f28d69972110e88a716dc9c
Tekeshwar Singh <tekeshwarsingh2000@gmail.com>
2023-04-04 03:19:36 +0530

    wip vendor profile edit

--- lib/presentation/pages/profile/profile_page.dart
+++ lib/presentation/pages/profile/profile_page.dart
@@ -1,6 +1,9 @@
 import 'package:admin/core/constant/colors.dart';
 import 'package:admin/core/constant/fontstyles.dart';
+import 'package:intl/intl.dart';
+import 'package:admin/core/enum/role.dart';
 import 'package:admin/presentation/pages/profile/profile_view_model.dart';
+import 'package:admin/presentation/pages/widgets/custom_textfield.dart';
 import 'package:admin/presentation/pages/widgets/footer.dart';
 import 'package:admin/presentation/pages/widgets/header.dart';
 import 'package:cached_network_image/cached_network_image.dart';
@@ -35,33 +38,129 @@ class _ProfilePageState extends ConsumerState<ProfilePage> {
           : ListView(
               children: [
                 const Header(mobile: false),
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.9,
-                  child: ListView(
-                    children: [
-                      ListTile(
-                        title: Text("Profile",
-                            style: FontStyles.font24Semibold.copyWith(
-                                fontSize: 48, color: AppColors.blueColor)),
-                        subtitle: Text("Your profile details are updated here",
-                            style: FontStyles.font12Medium
-                                .copyWith(color: AppColors.blueColor)),
-                      ),
-                      Row(
-                        children: [
-                          CircleAvatar(
-                            child: CachedNetworkImage(
-                                imageUrl: "", height: 200, width: 200),
-                          ),
-                          ListView(
-                            shrinkWrap: true,
-                            children: [
-                              Row(),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ],
+                Form(
+                  key: _viewModel.formKey,
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.9,
+                    child: ListView(
+                      children: [
+                        ListTile(
+                          title: Text("Profile",
+                              style: FontStyles.font24Semibold.copyWith(
+                                  fontSize: 48, color: AppColors.blueColor)),
+                          subtitle: Text(
+                              "Your profile details are updated here",
+                              style: FontStyles.font12Medium
+                                  .copyWith(color: AppColors.blueColor)),
+                        ),
+                        Row(
+                          children: [
+                            CircleAvatar(
+                              child: CachedNetworkImage(
+                                  imageUrl: "", height: 200, width: 200),
+                            ),
+                            ListView(
+                              shrinkWrap: true,
+                              children: [
+                                Row(
+                                  children: [
+                                    const Text("Name"),
+                                    CustomTextField(
+                                      hintText: "Your Name",
+                                      controller: _viewModel.nameController,
+                                    ),
+                                  ],
+                                ),
+                                Row(
+                                  children: [
+                                    const Text("Email"),
+                                    AbsorbPointer(
+                                      child: CustomTextField(
+                                        errorText: _viewModel.emailError,
+                                        hintText: "Your Email",
+                                        controller: _viewModel.emailController,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                Row(
+                                  children: [
+                                    const Text("Phone"),
+                                    CustomTextField(
+                                      errorText: _viewModel.phoneError,
+                                      hintText: "Your Phone number",
+                                      controller: _viewModel.phoneController,
+                                    ),
+                                  ],
+                                ),
+                                Visibility(
+                                  visible: _viewModel.getUser.userType ==
+                                      UserType.vendor,
+                                  child: Row(
+                                    children: [
+                                      const Text("Company Name"),
+                                      CustomTextField(
+                                        hintText: "Your Company Name",
+                                        controller:
+                                            _viewModel.companyNameController,
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                Visibility(
+                                  visible: _viewModel.getUser.userType ==
+                                      UserType.vendor,
+                                  child: Row(
+                                    children: [
+                                      const Text("Permanent Address"),
+                                      CustomTextField(
+                                        hintText: "Your Permanent Address",
+                                        controller: _viewModel
+                                            .permanentAddressController,
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                Visibility(
+                                  visible: _viewModel.getUser.userType ==
+                                      UserType.vendor,
+                                  child: Row(
+                                    children: [
+                                      const Text("Start Working Hour"),
+                                      InkWell(
+                                        onTap: () async {
+                                          TimeOfDay? pickedTime =
+                                              await showTimePicker(
+                                            initialTime: TimeOfDay.now(),
+                                            context: context,
+                                          );
+                                          if (pickedTime != null) {
+                                            _viewModel
+                                                    .startingWorkHourController
+                                                    .text =
+                                                DateFormat.jm()
+                                                    .parse(
+                                                        pickedTime.toString())
+                                                    .toString();
+                                          }
+                                        },
+                                        child: AbsorbPointer(
+                                          child: CustomTextField(
+                                            hintText: "Your Starting Work Time",
+                                            controller: _viewModel
+                                                .startingWorkHourController,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 ),
                 const Footer(),
--- lib/presentation/pages/profile/profile_view_model.dart
+++ lib/presentation/pages/profile/profile_view_model.dart
@@ -1,8 +1,10 @@
+import 'package:admin/core/extension/validator.dart';
 import 'package:admin/core/provider.dart';
 import 'package:admin/data/models/models.dart';
 import 'package:admin/data/models/vendor.dart';
 import 'package:admin/data/repositories/index.dart';
 import 'package:admin/presentation/base_view_model.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final _provider = ChangeNotifierProvider.autoDispose(
@@ -22,6 +24,110 @@ class ProfileViewModel extends BaseViewModel {
 
   User get getUser => _user;
   Vendor? get getVendor => _vendor;
+
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController phoneController = TextEditingController();
+  final TextEditingController companyNameController = TextEditingController();
+  final TextEditingController permanentAddressController =
+      TextEditingController();
+  final TextEditingController startingWorkHourController =
+      TextEditingController();
+  final TextEditingController endingWorkHourController =
+      TextEditingController();
+  final TextEditingController accountNumberController = TextEditingController();
+  final TextEditingController ifscController = TextEditingController();
+  // degree
+  final TextEditingController otherDegreeController = TextEditingController();
+  // university
+  final TextEditingController otherUniversityContoller =
+      TextEditingController();
+  final TextEditingController associateNameContoller = TextEditingController();
+  final TextEditingController associateAddressContoller =
+      TextEditingController();
+  final TextEditingController associatePermanentAddressController =
+      TextEditingController();
+  final TextEditingController qualifiedYearContoller = TextEditingController();
+  final TextEditingController practicingExperienceContoller =
+      TextEditingController();
+  final TextEditingController expertServicesContoller = TextEditingController();
+  final TextEditingController landlineContoller = TextEditingController();
+  final TextEditingController mobileContoller = TextEditingController();
+
+  String? emailError;
+  String? phoneError;
+  String? accountNumberError;
+  String? ifscError;
+  String? otherDegreeError;
+  String? otherUniversityError;
+  String? qualifiedYearError;
+  String? practicingExperienceError;
+  String? landlineError;
+  String? mobileError;
+
+  void clearError() {
+    emailError = phoneError = accountNumberError = ifscError =
+        otherDegreeError = otherUniversityError = qualifiedYearError =
+            practicingExperienceError = landlineError = mobileError = null;
+    notifyListeners();
+  }
+
+  bool validate() {
+    clearError();
+    if (emailController.text.isNotEmpty &&
+        !emailController.text.isValidEmail()) {
+      emailError = "Please enter a valid Email";
+    }
+    if (phoneController.text.isNotEmpty &&
+        !phoneController.text.isValidPhoneNumber()) {
+      phoneError = "Please enter a valid Phone number";
+    }
+    if (accountNumberController.text.isNotEmpty &&
+        int.tryParse(accountNumberController.text) == null) {
+      accountNumberError = "Please enter a valid Bank Account Number";
+    }
+    // other degree
+    // if (accountNumberController.text.isNotEmpty &&
+    //     int.tryParse(accountNumberController.text) == null) {
+    //   accountNumberError = "Please enter a valid Bank Account Number";
+    // }
+    // other university
+    // if (accountNumberController.text.isNotEmpty &&
+    //     int.tryParse(accountNumberController.text) == null) {
+    //   accountNumberError = "Please enter a valid Bank Account Number";
+    // }
+    if (qualifiedYearContoller.text.isNotEmpty &&
+        int.tryParse(qualifiedYearContoller.text) == null &&
+        int.parse(qualifiedYearContoller.text) < 1900) {
+      qualifiedYearError = "Please enter a valid Year";
+    }
+    if (practicingExperienceContoller.text.isNotEmpty &&
+        int.tryParse(practicingExperienceContoller.text) == null &&
+        int.parse(practicingExperienceContoller.text) > 0) {
+      practicingExperienceError = "Please enter a valid Experience in Months";
+    }
+    if (landlineContoller.text.isNotEmpty &&
+        !landlineContoller.text.isValidPhoneNumber()) {
+      landlineError = "Please enter a valid Landline number";
+    }
+    if (mobileContoller.text.isNotEmpty &&
+        !mobileContoller.text.isValidPhoneNumber()) {
+      mobileError = "Please enter a valid mobile number";
+    }
+    notifyListeners();
+    return emailError == null &&
+        phoneError == null &&
+        accountNumberError == null &&
+        ifscError == null &&
+        otherDegreeError == null &&
+        otherUniversityError == null &&
+        qualifiedYearError == null &&
+        practicingExperienceError == null &&
+        landlineError == null &&
+        mobileError == null;
+  }
 
   Future<void> fetchUser(String uid) async {
     toggleLoadingOn(true);
