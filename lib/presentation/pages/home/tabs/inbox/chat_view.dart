b3465daef19db081a41b8107de7a481e80f6053e
Manav <83003394+Manav5606@users.noreply.github.com>
2023-04-26 19:59:24 +0530

    latest
    latest

--- lib/presentation/pages/home/tabs/inbox/chat_view.dart
+++ lib/presentation/pages/home/tabs/inbox/chat_view.dart
@@ -23,15 +23,73 @@ class _ChatViewState extends ConsumerState<ChatView> {
       body: Container(
           // height: 500,
           child: Column(
-        // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Expanded(
-            child: ListView.builder(
-              itemCount: 5,
-              itemBuilder: (BuildContext context, int index) {
-                // final message = _messages[index];
-                return ListTile(title: Text("message"));
-              },
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width * 0.35,
+              child: ListView.builder(
+                itemCount: 5,
+                itemBuilder: (BuildContext context, int index) {
+                  final isMyMessage = index % 2 ==
+                      0; // alternate between my and other person's messages
+                  return Container(
+                    alignment: isMyMessage
+                        ? Alignment.centerLeft
+                        : Alignment.centerRight,
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        isMyMessage
+                            ? SizedBox(width: 48.0)
+                            : Container(
+                                width: 40.0,
+                                height: 40.0,
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  image: DecorationImage(
+                                    image: NetworkImage(
+                                        "https://example.com/other_user_image.jpg"),
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ),
+                        SizedBox(width: 8.0),
+                        Flexible(
+                          child: Container(
+                            padding: EdgeInsets.all(8.0),
+                            decoration: BoxDecoration(
+                              color: isMyMessage
+                                  ? AppColors.lightBlueColor
+                                  : Colors.pink[100],
+                              borderRadius: BorderRadius.circular(8.0),
+                            ),
+                            child: Text(
+                              "This is a ${isMyMessage ? "sent" : "received"} message with a ${isMyMessage ? "light blue" : "light red"} background",
+                              style: TextStyle(fontSize: 16.0),
+                            ),
+                          ),
+                        ),
+                        isMyMessage
+                            ? Container(
+                                width: 40.0,
+                                height: 40.0,
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  image: DecorationImage(
+                                    image: NetworkImage(
+                                        "https://example.com/my_user_image.jpg"),
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              )
+                            : SizedBox(width: 48.0),
+                      ],
+                    ),
+                  );
+                },
+              ),
             ),
           ),
           Container(
